1be89d19d0dbf4a58e167cabc2832ad42c72da4d
Byron Pearce <byront-800@protonmail.com>
2021-04-16 15:19:59 -0300

    Add score updating

--- game.rb
+++ game.rb
@@ -21,18 +21,19 @@ Rect = DefStruct.new{{
 end
 
 Obstacle = DefStruct.new{{
-  y: 0,
-  x: 0
+  pos: Vec[0, 0],
+  player_has_crossed: false,
 }}
 
 GameState = DefStruct.new{{
   started: false,
+  score: 0,
   alive: true,
   scroll_x: 0,
   player_pos: Vec[20,200],
   player_vel: Vec[0,0],
   player_rotation: 0,
-  obstacles: [], # Array of Vecs
+  obstacles: [], # Array of obstacles
   obstacle_countdown: OBSTACLE_SPAWN_INTERVAL,
 }}
 
@@ -41,6 +42,7 @@ class GameWindow < Gosu::Window
   def initialize(*args)
     super
     @scroll_x = 0
+    @font = Gosu::Font.new(self, Gosu.default_font_name, 30)
     @images = {
       background: Gosu::Image.new(self, 'images/background.png', false),
       foreground: Gosu::Image.new(self, 'images/foreground.png', true),
@@ -60,7 +62,8 @@ class GameWindow < Gosu::Window
   end
 
   def spawn_obstacle
-    @state.obstacles << Vec[width, rand(50...300)]
+    # @state.obstacles << Vec[width, rand(50...300)]
+    @state.obstacles << Obstacle.new(pos: Vec[width, rand(50..300)])
   end
 
   def update
@@ -80,11 +83,18 @@ class GameWindow < Gosu::Window
     if @state.obstacle_countdown <= 0
       spawn_obstacle
       @state.obstacle_countdown += OBSTACLE_SPAWN_INTERVAL
+      puts @state.obstacles.size
     end
 
     @state.obstacles.each do |obst|
-      obst.x -= dt * OBSTACLE_SPEED
+      obst.pos.x -= dt * OBSTACLE_SPEED
+      if obst.pos.x < @state.player_pos.x && !obst.player_has_crossed && @state.alive
+        @state.score += 1
+        obst.player_has_crossed = true
+      end
     end
+
+    @state.obstacles.reject! { |obst| obst.pos.x < -@images[:obstacle].width }
 
     if @state.alive && player_is_colliding?
       @state.alive = false
@@ -121,15 +131,15 @@ class GameWindow < Gosu::Window
     @state.obstacles.each do |obst|
       img_y = @images[:obstacle].height
       # top obstacle
-      @images[:obstacle].draw(obst.x, obst.y - img_y, 0)
+      @images[:obstacle].draw(obst.pos.x, obst.pos.y - img_y, 0)
       scale(1, -1) do
         # bottom obstacle
-        @images[:obstacle].draw(obst.x, -height - img_y + (height - obst.y - OBSTACLE_GAP), 0)
+        @images[:obstacle].draw(obst.pos.x, -height - img_y + (height - obst.pos.y - OBSTACLE_GAP), 0)
       end
     end
 
     @images[:player].draw_rot(@state.player_pos.x, @state.player_pos.y, 0, @state.player_rotation, 0, 0)
-
+    @font.draw_rel(@state.score.to_s, width/2.0, 60, 0, 0.5, 0.5)
     # debug_draw # toggle rect collision lines
   end
 
@@ -145,8 +155,8 @@ class GameWindow < Gosu::Window
     obst_size = Vec[@images[:obstacle].width, @images[:obstacle].height]
 
     @state.obstacles.flat_map do |obst|
-      top = Rect.new(pos: Vec[obst.x, obst.y - img_y], size: obst_size)
-      bottom = Rect.new(pos: Vec[obst.x, obst.y + OBSTACLE_GAP], size: obst_size)
+      top = Rect.new(pos: Vec[obst.pos.x, obst.pos.y - img_y], size: obst_size)
+      bottom = Rect.new(pos: Vec[obst.pos.x, obst.pos.y + OBSTACLE_GAP], size: obst_size)
       [top, bottom]
     end
   end
